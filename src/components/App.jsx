aa548119ff2a66a98b600e3564243b9576d5c98e
Hack Reactor Student <student@hackreactor.com>
2017-06-02 18:56:13 -0700

    videoList working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 0
+    };
+  }  
+  
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.props.videos[this.state.selected]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.props.videos} />
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// TODO: Render the `App` component to the DOM
+
+
+
+ReactDOM.render(<App videos={exampleVideoData} />, document.getElementById('app'));
+
+
 
